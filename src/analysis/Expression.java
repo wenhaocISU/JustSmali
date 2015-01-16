8b3c0de3a734573bd0f9d67cfb1bd5e0db7572f6
Wenhao Chen <xjtujack@gmail.com>
2015-01-16 14:06:00 -0600

    updated Expression class

--- src/analysis/Expression.java
+++ src/analysis/Expression.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 import javax.swing.tree.DefaultMutableTreeNode;
  
-public class Expression extends DefaultMutableTreeNode{ // 
+public class Expression extends DefaultMutableTreeNode{ 
 	public static int availableId = 0;
 	public final int creationId;
 	
@@ -30,6 +30,15 @@ public class Expression extends DefaultMutableTreeNode{ //
 	}
 	
 	public String toYicesStatement(){
+		if(this.isLeaf()){
+			String result = this.getUserObject().toString();
+			for(int index = 0; index < this.getChildCount(); index++){
+				Expression expr = (Expression) this.getChildAt(index);
+				result += " "+expr.toYicesStatement();
+			}
+			return result;
+		}
+		
 		String result = "("+this.getUserObject().toString();
 		for(int index = 0; index < this.getChildCount(); index++){
 			Expression expr = (Expression) this.getChildAt(index);
@@ -37,27 +46,7 @@ public class Expression extends DefaultMutableTreeNode{ //
 		}
 		return result + " )";
 	}
-	
-	//nextLeaf may not end !
-	
-//	public boolean contains(Variable var){
-//		DefaultMutableTreeNode current = this.getFirstLeaf();
-//		do{
-//			if(current.equals(var)) return true;
-//			current = current.getNextLeaf();
-//		}while(current != null);
-//		return false;
-//	}
-//	
-//	public boolean contains(String partten){
-//		Expression current = (Expression) this.getFirstLeaf();
-//		do{
-//			if(current.toYicesStatement().matches(partten)) return true;
-//			current = (Expression) current.getNextLeaf();
-//		}while(current != null);
-//		return false;
-//	}
-	
+
 	public Set<Variable> getUniqueVarSet(){
 		return getUniqueVarSet(null);
 	}
@@ -79,50 +68,6 @@ public class Expression extends DefaultMutableTreeNode{ //
 		}
 		
 		return varSet;
-		
-		
-		
-		
-		
-		
-//		Set<Variable> varSet = new HashSet<Variable>();
-//		
-//		System.out.println("count: "+this.getChildCount());
-//		Expression e1 = (Expression) this.getFirstLeaf();
-//		System.out.println(e1.creationId);
-//		Expression e2 = (Expression) e1.getNextLeaf();
-//		System.out.println(e2.creationId);
-//		Expression e3 = (Expression) e2.getNextLeaf();
-//		if(e3 != null) System.out.println(e3.creationId);
-//		System.out.println(e2==e3);
-//		
-//		if(this.isLeaf() ){
-//			if(this instanceof Variable){
-//				if(partten == null){
-//					varSet.add((Variable)this);
-//				}else if(this.toYicesStatement().matches(partten)){
-//					varSet.add((Variable)this);
-//				}
-//				return varSet;
-//			}else{
-//				return varSet;
-//			}
-//		}
-//		
-//		DefaultMutableTreeNode leaf = this.getFirstLeaf();
-//		do{
-//			if(leaf instanceof Variable){
-//				Variable var = (Variable)leaf;
-//				if(partten == null){
-//					varSet.add(var);
-//				}else if(var.toYicesStatement().matches(partten)){
-//					varSet.add(var);
-//				}
-//			}
-//			leaf = leaf.getNextLeaf();
-//			
-//		}while(leaf != null);
-//		return varSet;
 	}
 	
 	public boolean replace(String toReplace, Expression replacement){
@@ -175,7 +120,6 @@ public class Expression extends DefaultMutableTreeNode{ //
 		}
 		return false;
 	}
-	
 	
 	public static Set<Variable> getUnqiueVarSet(Collection<? extends Expression> inputs){
 		Set<Variable> result = new HashSet<Variable>();
